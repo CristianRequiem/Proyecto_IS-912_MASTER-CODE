0096591e9050c08c6efa26c86d6c03e9e85978a7
CristianRequiem <cristianrequiem@yahoo.es>
2019-04-23 00:50:41 -0600

    Actualización

--- MASTER CODE/index.js
+++ MASTER CODE/index.js
@@ -3,15 +3,18 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var dbConexion = require('./config/conexion_DB');
+
 //Crear una aplicacion de nodejs con express
 var app = express();
 
+//middleware
+//app.use(morgan('dev'));
 
 //definir una carpeta como publica para que los usuarios puedan acceder a su contenido
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
- 
+
 //Levantar el servidor en el puerto 3333
 app.listen(3333, function(){
     console.log("Servidor levantado en el puerto 3333");
